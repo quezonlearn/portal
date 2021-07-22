26403f528f71a05c426e56da2b8a63afc6084b83
cyberpau <johnpaulomataac@gmail.com>
2021-07-22 21:03:57 +0800

    initial files

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+
+// Load config files
+dotenv.config({ path: './config/config.env'})
+
+connectDB()
+
+const app = express()
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+// Handlebars
+app.engine('.hbs', exphbs({defaultLayout: 'main', extname: '.hbs'}))
+app.set('view engine', '.hbs')
+
+// Routes
+app.use('/', require('./routes/index'))
+
+const PORT = process.env.PORT || 3000
+
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
